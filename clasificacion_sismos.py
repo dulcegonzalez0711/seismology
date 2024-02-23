5b28ccea5d3d97fefd9663b9e5c72b0afe663f25
dulcegonzalez0711 <91688348+dulcegonzalez0711@users.noreply.github.com>
2024-02-23 09:29:42 -0600

    Add files via upload

---
+++ clasificacion_sismos.py
@@ -0,0 +1,31 @@
+#Dulce Abril González 
+# Introducción al programa
+print("********************************************")
+print("*  Bienvenido a la Clasificación de Sismos  *")
+print("*    en Función de su Profundidad (CSP)    *")
+print("********************************************")
+print("")
+print(" Para la clasificación se utilizan los siguientes parámetros:")
+print(" 0 < h ≤ 25   ---   Cortical")
+print(" 25 < h ≤ 60  ---   Subducción interfase")
+print(" h > 60       ---   Subducción intraplaca")
+print("")
+
+
+import pandas as pd
+
+# Cargar el archivo CSV en un DataFrame
+ruta = r'C:\Users\LEGION\Desktop\sismología\Data\reporte_info_all_2023.csv'
+df = pd.read_csv(ruta)
+
+# Crear una nueva columna "tipo" basada en la columna "depth"
+df['clasificación'] = df['DEPTH'].apply(lambda x: 'cortical' if x <= 25 else ('subducción interfase' if x > 25 and x <= 60 else 'subducción intraplaca'))
+
+# Guardar el DataFrame modificado en un nuevo archivo CSV
+ruta_nueva = r'C:\Users\LEGION\Desktop\sismología\Data\datos_modificados.csv'
+df.to_csv(ruta_nueva, index=False)
+
+#mostrarle al usuario el resultado de la clasificación
+print("A continuación una pequeña muestra que recopila columnas de identificación y aquellas usadas para hacer la clasificación en este script. :)")
+print("")
+print(df[['NO', 'ID', 'DEPTH', 'clasificación']].head())
