d6efcd7b7843f64f258baef022c9a1ba5edffbe1
dulcegonzalez0711 <91688348+dulcegonzalez0711@users.noreply.github.com>
2024-02-23 09:31:59 -0600

    Update clasificacion_sismos.py

--- clasificacion_sismos.py
+++ clasificacion_sismos.py
@@ -15,14 +15,14 @@ print("")
 import pandas as pd
 
 # Cargar el archivo CSV en un DataFrame
-ruta = r'C:\Users\LEGION\Desktop\sismología\Data\reporte_info_all_2023.csv'
+ruta = r'reporte_info_all_2023.csv'
 df = pd.read_csv(ruta)
 
 # Crear una nueva columna "tipo" basada en la columna "depth"
 df['clasificación'] = df['DEPTH'].apply(lambda x: 'cortical' if x <= 25 else ('subducción interfase' if x > 25 and x <= 60 else 'subducción intraplaca'))
 
 # Guardar el DataFrame modificado en un nuevo archivo CSV
-ruta_nueva = r'C:\Users\LEGION\Desktop\sismología\Data\datos_modificados.csv'
+ruta_nueva = r'reporte_info_all_2023_clasificados.csv'
 df.to_csv(ruta_nueva, index=False)
 
 #mostrarle al usuario el resultado de la clasificación
